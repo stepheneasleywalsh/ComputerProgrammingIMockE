edf66e0490dd255f06263918596726aef1481f11
Stephen Easley-Walsh <stephen.easley-walsh@studygroup.com>
2023-11-22 22:41:29 +0000

    Create tutorial6.py

---
+++ tutorial6.py
@@ -0,0 +1,40 @@
+# Student Number 00000000
+
+# Define function
+def f(x):
+    return(-x**2+3*x-2)
+
+
+# Print table of values
+print(" # # # f(x) = -x^2 + 3x + 2 from a=1 to b=2 # # # ")
+x = 1.0
+middlesum = 0.0
+while x < 2.01:
+    print("When x =",x,"=> f(x) =", f(x))
+    middlesum += f(x)
+    x += 0.10
+
+
+# Compute first, last and middle sum
+first = f(1)
+last = f(2)
+middlesum -= first
+middlesum -= last
+
+
+# Results of Trapezium Rule with error
+h = 0.10
+T = (h/2)*(first + 2*middlesum + last)
+I = 1/6
+
+print(" # # # Example of Trapezium Rule Values # # # ")
+print("First Height:", first)
+print("Last Height:", last)
+print("Middle Sum:", middlesum)
+print("Integration is approximately", T)
+print("True value of integration is 1/6")
+error = 100*(I-T)/T
+print("Therefore the error is ",error,"%")
+
+# QUIT
+quit()
