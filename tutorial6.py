48006329a7b569bebf47a2335cdfaec0a0c0bdba
Stephen Easley-Walsh <stephen.easley-walsh@studygroup.com>
2024-03-08 14:19:48 +0000

    Update tutorial6.py

--- tutorial6.py
+++ tutorial6.py
@@ -4,26 +4,30 @@
 def f(x):
     return(-x**2+3*x-2)
 
+# h, a and b
+h = 0.10
+a = 1
+b = 2
 
 # Print table of values
 print(" # # # f(x) = -x^2 + 3x - 2 from a=1 to b=2 # # # ")
-x = 1.0
-middlesum = 0.0
-while x < 2.01:
+x = a
+while x < b+h/2:
     print("When x =",x,"=> f(x) =", f(x))
-    middlesum += f(x)
-    x += 0.10
+    x += h
 
 
 # Compute first, last and middle sum
-first = f(1)
-last = f(2)
-middlesum -= first
-middlesum -= last
+first = f(a)
+last = f(b)
+x = a+h # Don't start at a=1, start at 1.1
+middlesum = 0
+while x < b: # finish at 1.9
+    middlesum += f(x)
+    x += h
 
 
 # Results of Trapezium Rule with error
-h = 0.10
 ApproxValue = (h/2)*(first + 2*middlesum + last)
 TrueValue = 1/6
 
